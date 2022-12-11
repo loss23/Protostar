467e0f64c4e3495089925b4c2c0105621512ba3a
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-11 17:42:48 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,3 +1,7 @@
+if not syn then    
+    game.Players.LocalPlayer:Kick("You gotta use synapse for this script broke bitch")
+end
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/loss23/Protostar/main/S1/CodeShit.lua",true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/loss23/Protostar/main/S1/KeyGen.lua",true))()
 

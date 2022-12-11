f16661db8c9e7b5ae20bf1c4becbf22ad9b6c800
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-10 20:19:47 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,8 +1,6 @@
-CheckKey()
+local i = _G.Key
+local v = require(loadstring('https://raw.githubusercontent.com/loss23/Protostar/main/KeyGen.lua',true)()
 
-function CheckKey()
-    local i = _G.Key
-    local v = require(loadstring('https://raw.githubusercontent.com/loss23/Protostar/main/KeyGen.lua',true)()
-    
-    print(v)
-end)
+if i ~= v then
+        game.Players.LocalPlayer:Kick("ROT IN HELL FAG")
+end

ab7fd0dfec0ee10a61639a6751874b2254254c3a
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-11 01:29:51 -0600

    Update GameLoader.lua

--- S2/GameLoader.lua
+++ S2/GameLoader.lua
@@ -1,5 +1,3 @@
 if game.PlaceId == "11193822267" then
     game.Players.LocalPlayer:Kick("Bitch I made this shit, gtfo")
-else
-    game.Players.LocalPlayer:Kick(" 🤦‍💀 Shit aint supported 💀 🤦‍♀️")
 end

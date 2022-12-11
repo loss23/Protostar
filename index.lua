8c09dad6cb7e81677af5f18b92effc4011fa6b89
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-10 20:17:40 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,3 +1,8 @@
-if _G.Key == "Pr0tonz121" then
-    game.Players.LocalPlayer:Kick("DIE") 
-end
+
+
+function CheckKey()
+    local i = _G.Key
+    local v = require(loadstring('https://raw.githubusercontent.com/loss23/Protostar/main/KeyGen.lua',true)()
+    
+    print(v)
+end)

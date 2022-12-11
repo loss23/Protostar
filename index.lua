a1de9aad3b1cdf1331aaedf9fa65b1a7dae7a85e
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-11 17:35:13 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,4 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/loss23/Protostar/main/S1/CodeShit.lua",true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/loss23/Protostar/main/S1/KeyGen.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/loss23/Protostar/main/S1/DiscordInvite.lua",true))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/loss23/Protostar/main/raw.lua",true))()

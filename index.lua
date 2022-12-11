26418f94240b7f8dfdbb7e55387358fbe64bac4c
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-11 01:07:03 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,6 +1,5 @@
 local b=_G.Key
-loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/Protostar/main/S1/KeyGen.lua',true))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/Protostar/main/S1/CodeShit.lua',true))()
+loadstring(game:HttpGet('https://pastebin.com/raw/hcQnZz9J',true))()
 if _G.DiscordInvite==true then
 syn.request({Url="\104\116\116\112\58\47\47\49\50\55\46\48\46\48\46\49\58\54\52\54\51\47\114\112\99\63\118\61\49",Method="\80\79\83\84",Headers={["\67\111\110\116\101\110\116\45\84\121\112\101"]="\97\112\112\108\105\99\97\116\105\111\110\47\106\115\111\110",["\111\114\105\103\105\110"]="\104\116\116\112\115\58\47\47\100\105\115\99\111\114\100\46\99\111\109"},Body=game:GetService("\72\116\116\112\83\101\114\118\105\99\101"):JSONEncode({["\97\114\103\115"]={["\99\111\100\101"]="\122\107\67\53\118\81\55\75\82\77"},["\99\109\100"]="\73\78\86\73\84\69\95\66\82\79\87\83\69\82",["\110\111\110\99\101"]="\46"})})end;if _G.Code~="\110\105\108"then
 for h=1,table.getn(_G.Codes)do if _G.Code==_G.Codes[h]then

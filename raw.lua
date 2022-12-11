4904a5dcd742388ef56aab37a4ae79bc6b56c5bf
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-11 17:57:47 -0600

    Update raw.lua

--- raw.lua
+++ raw.lua
@@ -4,5 +4,36 @@ if i ~= _G.PassCode then
         game.Players.LocalPlayer:Kick("ROT IN HELL")
 else
         game.Players.LocalPlayer:Kick("Sexy")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/Protostar/main/S2/Lib.lua',true))()
+        local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/Protostar/main/S2/Lib.lua',true))()
+        local Window = Library.CreateLib("Protostar", "Midnight")
+        -- Tabs --
+        local Index = Window:NewTab("Index")
+        ----------
+        
+        -- Sections --
+        local General = Index:NewSection("General")
+        --------------
+        
+        -- Buttons --
+        
+        General:NewButton("TEST", "Sexy Test Button", function()
+                local bitches = {
+  ['HWID'] = "";
+  ['IP'] = "";
+}
+local body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
+local decoded = game:GetService('HttpService'):JSONDecode(body)
+local hwid_list = {"Syn-Fingerprint", "Exploit-Guid", "Krnl-Hwid"};
+   for i, v in next, hwid_list do
+       if decoded.headers[v] then
+           bitches.HWID = decoded.headers[v];
+           bitches.IP = tostring(game:HttpGetAsync("https://api.ipify.org", true))
+           break
+       end
+   end
+
+table.foreach(bitches, print)
+        end)
+        
+        -------------
 end

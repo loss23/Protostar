efe79cefcc11498bb2d411599896e7a2ab6b201c
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-10 20:29:17 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,5 +1,5 @@
 local i = _G.Key
-loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/Protostar/main/KeyGen.lua'),true)()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/Protostar/main/KeyGen.lua',true))()
 
 if i ~= _G.PassCode then
         game.Players.LocalPlayer:Kick("ROT IN HELL FAG")

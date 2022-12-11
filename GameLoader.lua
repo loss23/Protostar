51513f67029a651241ac813fdfc5170e0282465e
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-10 21:17:37 -0600

    Create GameLoader.lua

---
+++ GameLoader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == "11193822267" then
+    game.Players.LocalPlayer:Kick("Bitch I made this shit, gtfo")
+else
+    game.Players.LocalPlayer:Kick(" 🤦‍💀 Shit aint supported 💀 🤦‍♀️")
+end

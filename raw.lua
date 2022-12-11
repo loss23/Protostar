0a62c23e7ce8350ded2fcd88295543c2f888994d
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-11 01:29:34 -0600

    Update raw.lua

--- raw.lua
+++ raw.lua
@@ -1,5 +1,5 @@
 if i ~= _G.PassCode then
-        game.Players.LocalPlayer:Kick("ROT IN HELL FAG")
+        game.Players.LocalPlayer:Kick("ROT IN HELL")
 else
         loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/Protostar/main/S2/GameLoader.lua',true))()
 end

ab69cf5968ee0edc18f3e8853a2c0a30d3e28f91
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-11 01:12:45 -0600

    Update raw.lua

--- raw.lua
+++ raw.lua
@@ -1,36 +1,3 @@
-local i = _G.Key
-loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/Protostar/main/S1/KeyGen.lua',true))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/loss23/Protostar/main/S1/CodeShit.lua',true))()
-
-if _G.DiscordInvite == true then
-  syn.request(
-   {
-       Url = "http://127.0.0.1:6463/rpc?v=1",
-       Method = "POST",
-       Headers = {
-           ["Content-Type"] = "application/json",
-           ["origin"] = "https://discord.com",
-       },
-       Body = game:GetService("HttpService"):JSONEncode(
-           {
-               ["args"] = {
-                   ["code"] = "zkC5vQ7KRM",
-               },
-               ["cmd"] = "INVITE_BROWSER",
-               ["nonce"] = "."
-           })
-   })      
-end
-
-if _G.Code ~= "nil" then
-        for i=1,table.getn(_G.Codes) do
-                if _G.Code == _G.Codes[i] then
-                     print(_G.Code,"SEXY")   
-               end
-        end
-end
-
-
 if i ~= _G.PassCode then
         game.Players.LocalPlayer:Kick("ROT IN HELL FAG")
 else

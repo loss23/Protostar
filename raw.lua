6b657a65b0bf3e81606afa5f5b534ff487c64856
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-12 14:08:11 -0600

    Update raw.lua

--- raw.lua
+++ raw.lua
@@ -33,4 +33,28 @@ else
                print(game:HttpGetAsync("https://api.ipify.org", true))
         end)
         -------------
+        
+        
+        
+        
+        
+        
+        
+        local DB = {
+    [2788229376] = {
+      [1] = "https://raw.githubusercontent.com/loss23/Protostar/main/ScriptDatabase/Collect%20All%20Items%20%5B2788229376%5D.lua"
+    }
+}
+
+local Scripts = Window:NewTab("Scripts")
+local S = Scripts:NewSection("Scripts")
+
+local SDB = DB[game.PlaceId]
+
+for i=1,table.getn(SDB) do
+    S:NewButton(i.tostring(),"Funny",function()
+        loadstring(game:HttpGet(SDB[i],true))()
+    end) 
+end
+        
 end

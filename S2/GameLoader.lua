25a32e2b6bf87913cabb1f5c48a8b700d8e81834
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-12 14:03:35 -0600

    Update GameLoader.lua

--- S2/GameLoader.lua
+++ S2/GameLoader.lua
@@ -1,1 +1,16 @@
-print("Games Coming Soon CUNT BITCH HOE")
+local DB = {
+    [2788229376] = {
+      [1] = "https://raw.githubusercontent.com/loss23/Protostar/main/ScriptDatabase/Collect%20All%20Items%20%5B2788229376%5D.lua"
+    }
+}
+
+local Scripts = Window:NewTab("Scripts")
+local S = Scripts:NewSection("Scripts")
+
+local SDB = DB[game.PlaceId]
+
+for i=1,table.getn(SDB) do
+    S:NewButton(i.tostring(),"Funny",function()
+        loadstring(game:HttpGet(SDB[i],true))()
+    end) 
+end
